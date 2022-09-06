6f50c08e3b5589ba42b747415a6a57a06561faf7
Emrullah AKTAŞ <111842183+AKTASEmrullah@users.noreply.github.com>
2022-09-06 09:31:43 +0300

    Girilen Sayının Mutlak Değerini Alma

---
+++ Girilen_Sayinin_Mutlak_Degerini_Alma/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Number;
+
+    cout << "Bir Sayı Girin: ";
+    cin >> Number;
+    cout << "Girilen Sayının Mutlak Değeri: " << abs(Number) << endl;;
+
+    return 0;
+}
