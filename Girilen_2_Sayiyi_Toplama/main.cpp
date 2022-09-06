295e9486889456a0a5aa24a053a2a7fee7be038a
Emrullah AKTAŞ <111842183+AKTASEmrullah@users.noreply.github.com>
2022-09-06 15:54:31 +0300

    Girilen 2 Sayıyı Toplama

---
+++ Girilen_2_Sayiyi_Toplama/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Number[2];
+
+    cout << "1. Sayıyı Girin: ";
+    cin >> Number[0];
+    cout << "2. Sayıyı Girin: ";
+    cin >> Number[1];
+    cout << "Sayıların Toplamı: " << Number[0] + Number[1] << endl;
+
+
+    return 0;
+}
