5fe23583b5c7ecd0b96f5d98d26dcaaa5c0f7231
Emrullah AKTAŞ <111842183+AKTASEmrullah@users.noreply.github.com>
2022-09-07 09:18:05 +0300

    Ekrana Adınızı Soyadınızı ve Doğum Tarihinizi Yazdırma

---
+++ Ekrana_Adinizi_Soyadinizi_ve_Dogum_Tarihinizi_Yazdirma/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string Name = "Emrullah";
+    string Sur_Name = "AKTAŞ";
+    string Birthday = "04.11.2002";
+
+    cout << "Merhaba, Ben " << Name << " " << Sur_Name << endl;
+    cout << Birthday << " Doğumluyum." << endl;
+
+    return 0;
+}
