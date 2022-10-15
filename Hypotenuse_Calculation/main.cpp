aa29bad267cd680c3b092dcfee4bf2cda54be254
Emrullah AKTAŞ <111842183+AKTASEmrullah@users.noreply.github.com>
2022-10-15 13:35:18 +0300

    Hipotenüs Hesaplama

---
+++ Hypotenuse_Calculation/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float Side[3];
+
+    cout << "1. Kenar: ";
+    cin >> Side[0];
+    cout << "2. Kenar: ";
+    cin >> Side[1];
+
+    Side[2] = sqrt(pow(Side[0], 2) + pow(Side[1], 2));
+    cout << "Hipotenus: " << Side[2] << endl;
+
+    return 0;
+}
